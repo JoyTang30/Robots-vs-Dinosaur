188dc257a8e8cdd57599c9892a2f75892117742a
unknown <jocelynlemer@yahoo.com>
2022-12-03 20:40:53 -0600

    Files dinosaur, robot, weapon, battlefiled, and main.py have been created

---
+++ robot.py
@@ -0,0 +1,10 @@
+class Robot:
+
+    def __init__(self, name):
+        self.name = name
+        self.health = int
+        self.active_weapon = " "
+
+
+    def attack(self, dinosaur):
+        pass
